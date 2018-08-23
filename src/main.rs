67fcefa4c4c3c3ea515ec6414cc412e6d215cc71
Paul Kinsky <pkinsky@gmail.com>
2018-08-23 14:18:11 -0700

    echo server with debug logging

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,42 @@ use std::thread;
 use std::os::unix::net::{UnixStream, UnixListener};
 use std::io::prelude::*;
 use std::net::Shutdown;
+use std::io::Read;
+use std::str;
 
-
+// todo: echo all text instead of insta-shutdown
 fn handle_client(mut socket: UnixStream) {
-    socket.write_all(b"hello world").unwrap();
-    socket.write_all(b"bye bye now").unwrap();
-    socket.shutdown(Shutdown::Both).expect("shutdown function failed");
+    socket.write_all(b"echo server sez: hello world").unwrap();
+
+    // reused on each iteration of loop
+    let mut buffer = [0; 64];
+
+    loop {
+
+      // read up to 64 bytes
+      match socket.read(&mut buffer[..]) {
+          Ok(n) => {
+              // todo expose failure possiblity?
+              // Q: why does that need '&'? It's not like it mutates the buffer..
+              // Guess: it's just transfering ownership, provides guarantee that, eg, no other thread
+              // can nuke the buffer while its in use
+              let s = str::from_utf8(&buffer[0..n]).unwrap();
+              println!("got string from stream: {}", s);
+
+              //TODO handle failure
+              socket.write(&buffer[0..n]).unwrap();
+          }
+          Err (err) => {
+              println!("reading from stream failed, err: {}", err);
+              socket.write_all(b"bye bye now").unwrap();
+              // TODO: magic string that triggers this
+              socket.shutdown(Shutdown::Both).expect("shutdown function failed");
+              break;
+          }
+      }
+    }
+
+
 }
 
 
@@ -34,6 +64,6 @@ fn main() {
       }
   }
 
-
+  // todo: catch CTRL-C and do cleanup
   println!("Hello, world!");
 }
